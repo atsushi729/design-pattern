23e768e2a73ab3cb0535e8e2cad44f8309b0bfa5
Atsushi <atu729@icloud.com>
2022-12-15 23:33:38 +0900

    add iterator

---
+++ iterator/index.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Iterator;
+
+
+class AlphabeticalOrderIterator implements \Iterator
+{
+    /**
+     * @var WordsCollection
+     */
+    private $collection;
+
+    /**
+     * @var int Stores the current traversal position. An iterator may have a
+     * lot of other fields for storing iteration state, especially when it is
+     * supposed to work with a particular kind of collection.
+     */
+    private $position = 0;
+
+    /**
+     * @var bool This variable indicates the traversal direction.
+     */
+    private $reverse = false;
+
+    public function __construct($collection, $reverse = false)
+    {
+        $this->collection = $collection;
+        $this->reverse = $reverse;
+    }
+
+    public function rewind()
+    {
+        $this->position = $this->reverse ?
+            count($this->collection->getItems()) - 1 : 0;
+    }
+
+    public function current()
+    {
+        return $this->collection->getItems()[$this->position];
+    }
+
+    public function key()
+    {
+        return $this->position;
+    }
+
+    public function next()
+    {
+        $this->position = $this->position + ($this->reverse ? -1 : 1);
+    }
+
+    public function valid()
+    {
+        return isset($this->collection->getItems()[$this->position]);
+    }
+}
+
+class WordsCollection implements \IteratorAggregate
+{
+    private $items = [];
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    public function addItem($item)
+    {
+        $this->items[] = $item;
+    }
+
+    public function getIterator(): Iterator
+    {
+        return new AlphabeticalOrderIterator($this);
+    }
+
+    public function getReverseIterator(): Iterator
+    {
+        return new AlphabeticalOrderIterator($this, true);
+    }
+}
+
+$collection = new WordsCollection();
+$collection->addItem('first');
+$collection->addItem('second');
+$collection->addItem('third');
+
+echo "Straight traversal:\n";
+foreach ($collection->getIterator() as $item) {
+    echo $item . "\n";
+}
+
+echo "\n";
+echo "Reverse traversal:\n";
+
+foreach ($collection->getReverseIterator() as $item) {
+    echo $item . "\n";
+}
